2db984a6fd28faada11843403df2de43e9f4f80d
Jérémy Lara <jeremy.lara@wanadoo.fr>
2018-04-04 22:48:19 +0200

    Folder architecture upadted, session 4 (first part) added

---
+++ TD_Work/Session4/SrcEvents_WCF_WS/SrcEventsLibWS/ICalcService.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+namespace EventsLib
+{
+    [ServiceContract(CallbackContract = typeof(ICalcServiceEvents))]
+    public interface ICalcService
+    {
+        [OperationContract]
+        void Calculate(int nOp, double dblNum1, double dblNum2);
+
+        [OperationContract]
+        void SubscribeCalculatedEvent();
+
+        [OperationContract]
+        void SubscribeCalculationFinishedEvent();
+    }
+
+}
---
+++ TD_Work/Session4/SrcEvents_WCF_WS/SrcEventsLibWS/ICalcServiceEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace EventsLib
+{
+    public interface ICalcServiceEvents
+    {
+        [OperationContract(IsOneWay = true)]
+        void Calculated(int nOp, double dblNum1, double dblNum2, double dblResult);
+
+        [OperationContract(IsOneWay = true)]
+        void CalculationFinished();
+    }
+}
